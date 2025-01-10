900e3f592c70cbbcbafe52b56dfb27f5dd20d117
mauu <mauriciord2004@gmail.com>
2025-01-10 02:54:18 -0500

    Aplicacion en Idioma Español

---
+++ lang/es/auth.php
@@ -0,0 +1,9 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+    'failed'   => 'Las credenciales son invalidas.',
+    'password' => 'La contraseña es incorrecta.',
+    'throttle' => 'Demasiados intentos de acceso. Por favor intente nuevamente en :seconds segundos.',
+];

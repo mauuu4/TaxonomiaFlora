6172bf481fd0f7e1c9caf3d598d31cb08e8e1d6e
mauu <mauriciord2004@gmail.com>
2025-02-01 17:51:19 -0500

    leaflet styles

--- resources/js/app.js
+++ resources/js/app.js
@@ -6,6 +6,15 @@ import imageUploader from './components/image-uploader';
 
 import 'leaflet/dist/leaflet.css';
 import L from 'leaflet';
+
+// Solucionar el problema de los íconos en producción
+delete L.Icon.Default.prototype._getIconUrl;
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon-2x.png',
+  iconUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon.png',
+  shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-shadow.png',
+});
+
 window.L = L;
 
 window.Alpine = Alpine;
